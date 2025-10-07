396fdc2fee25b6cc34feba4bd9c6ffc723142756
Jon-Marc Redwood <karjon.redwood@gmail.com>
2025-10-07 18:21:45 -0400

    Create Action.cs

---
+++ TerminalGameWithAudio/Action.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalGameWithAudio
+{
+    class Action
+    {
+        String[] aliases;
+        
+        public Action(String[] aliases) 
+        {
+            this.aliases = aliases;
+        }
+    }
+}
