ce00be3f584f118a59961f83495a9157b82b9e06
Jon-Marc Redwood <karjon.redwood@gmail.com>
2025-10-07 10:54:43 -0400

    Setup Project

---
+++ TerminalGameWithAudio/TerminalGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MohawkTerminalGame
+{
+    public class TerminalGame
+    {
+        // Place your variables here
+
+        /// Run once before Execute begins
+        public void Setup()
+        {
+            // Program configuration
+            Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteLoop;
+            Program.TerminalInputMode = TerminalInputMode.KeyboardReadAndReadLine;
+
+            // Hide raylib console output
+            Terminal.ForegroundColor = ConsoleColor.Black;
+            Terminal.CursorVisible = false;
+            Audio.Initialize();
+            
+            // Move curosr to overwrite previously drawn (black) text
+            Terminal.SetCursorPosition(0, 0);
+            Terminal.ResetColor();
+            Terminal.CursorVisible = true;
+        }
+
+        // Execute() runs based on Program.TerminalExecuteMode (assign to it in Setup).
+        //  ExecuteOnce: runs only once. Once Execute() is done, program closes.
+        //  ExecuteLoop: runs in infinite loop. Next iteration starts at the top of Execute().
+        //  ExecuteTime: runs at timed intervals (eg. "FPS"). Code tries to run at Program.TargetFPS.
+        //               Code must finish within the alloted time frame for this to work well.
+        public void Execute()
+        {
+            
+        }
+    }
+}
